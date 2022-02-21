15686b1573b8e59b2d08333043cd7ad46719c8b3
Hyewon Ryu <akuby1997@gmail.com>
2022-02-21 16:54:14 +0900

    easy

---
+++ 2475.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+let answer = 0;
+for(let i = 0; i < input.length; i++){
+  answer += parseInt(input[i]) ** 2
+}
+
+console.log(answer % 10);
